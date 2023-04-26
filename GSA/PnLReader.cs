7d285d823889619e8a7e71ca60610f9cf6914f4b
alomoa <muhammad.alam.97@outlook.com>
2023-04-26 22:35:57 +0100

    Move logic to an object class

---
+++ GSA/PnLReader.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace gsa
+{
+    internal class PnLReader
+    {
+        public PnLReader()
+        {
+        }
+
+        internal List<StrategyPnl> Read(string path)
+        {
+            List<StrategyPnl> strategyPnls = new List<StrategyPnl>();
+            using (TextFieldParser parser = new TextFieldParser("pnl.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                string[] headers = parser.ReadFields();
+                headers = headers.Skip(1).ToArray();
+
+                for (var i = 0; i < headers.Length; i++)
+                {
+                    var strategyPnln = new StrategyPnl
+                    {
+                        Strategy = headers[i],
+                        Pnls = new List<Pnl>()
+                    };
+                    strategyPnls.Add(strategyPnln);
+                }
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+
+                    var date = fields[0];
+                    fields = fields.Skip(1).ToArray();
+
+                    for (var i = 0; i < fields.Length; i++)
+                    {
+                        var pnl = new Pnl
+                        {
+                            Amount = decimal.Parse(fields[i]),
+                            Date = DateTime.Parse(date)
+                        };
+                        strategyPnls[i].Pnls.Add(pnl);
+                    }
+                }
+            }
+            return strategyPnls;
+        }
+    }
+}
--- GSA/Program.cs
+++ GSA/Program.cs
@@ -1,54 +1,12 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System.Data;
-using System.Net.Http.Headers;
-using static gsa.program;
-
-namespace gsa
+﻿namespace gsa
 {
     public class program
     {
         public static void Main(string[] args)
         {
-            List<StrategyPnl> strategyPnls = new List<StrategyPnl>();
+            PnLReader pnLReader = new();
 
-            using (TextFieldParser parser = new TextFieldParser("pnl.csv"))
-            {
-                parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters(",");
-                
-                string[] headers = parser.ReadFields();
-                headers = headers.Skip(1).ToArray();
-
-                
-
-                for (var i = 0; i < headers.Length; i++)
-                {
-                    var strategyPnln = new StrategyPnl
-                    {
-                        Strategy = headers[i],
-                        Pnls = new List<Pnl>()
-                    };
-                    strategyPnls.Add(strategyPnln);
-                }
-
-                while (!parser.EndOfData)
-                {
-                    string[] fields = parser.ReadFields();
-
-                    var date = fields[0];
-                    fields = fields.Skip(1).ToArray();    
-
-                    for (var i = 0; i < fields.Length; i++)
-                    {
-                        var pnl = new Pnl
-                        {
-                            Amount = decimal.Parse(fields[i]),
-                            Date = DateTime.Parse(date)
-                        };
-                        strategyPnls[i].Pnls.Add(pnl);
-                    }
-                }
-            }            
+            List<StrategyPnl> strategyPnls = pnLReader.Read("pnl.csv");            
         }
     }
 }
