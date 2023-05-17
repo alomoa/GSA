76f986b9b1357f4ead029fd6bf096f12162d715b
Alom <alom@lothea.co.uk>
2023-05-17 21:55:59 +0100

    combine cvs readers into one class

---
+++ GSA/StrategyReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSA
+{
+    public class StrategyReader
+    {
+        public List<Strategy> _strategies;
+
+        public StrategyReader() { 
+            var pnlReader = new PnLReader();
+            var capitalReader = new CapitalReader();
+            var propertiesReader = new PropertiesReader();
+
+            _strategies = pnlReader.ReadAndPopuplate();
+            capitalReader.ReadAndPopulate(_strategies);
+            propertiesReader.ConvertProperties(_strategies);
+        }
+    }
+}
