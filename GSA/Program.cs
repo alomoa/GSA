f46cfcdbd12a9ec5d55669c48dab6fe3022765f5
Alom <alom@lothea.co.uk>
2023-05-17 21:56:17 +0100

    update program with new class

--- GSA/Program.cs
+++ GSA/Program.cs
@@ -6,56 +6,9 @@ namespace GSA
     {
         public static void Main(string[] args)
         {
-            PnLReader pnLReader = new();
-            PropertiesReader propertiesReader = new PropertiesReader();
+            var strategyReader = new StrategyReader();
 
-            List<Strategy> strategyPnls = pnLReader.Execute("pnl.csv");
-            propertiesReader.ConvertProperties(strategyPnls);
-
-            foreach (var strategy in strategyPnls)
-            {
-                Console.WriteLine(strategy.StratName);
-            }
-
-            using (var db = new PnlContext())
-            {
-                db.Pnls.RemoveRange(db.Pnls);
-                db.StrategyPnls.RemoveRange(db.StrategyPnls);
-                db.SaveChanges();
-            }
-
-
-            foreach (var strategy in strategyPnls)
-            {
-                using (var db = new PnlContext())
-                {
-                    var dBStrategyPnl = new GSA.Data.Entity.StrategyPnl() { 
-                        Strategy = strategy.StratName,
-                    };
-                    db.StrategyPnls.Add(dBStrategyPnl);
-                    db.SaveChanges();
-
-                    var dBPnls = new List<GSA.Data.Entity.Pnl>();
-                    foreach (var pnl in strategy.Pnl)
-                    {
-                        var dBPnl = new GSA.Data.Entity.Pnl()
-                        {
-                            Date = pnl.Date,
-                            Amount = pnl.Amount,
-                            StrategyPnlId = dBStrategyPnl.Id
-                        };
-                        db.Pnls.Add(dBPnl);
-                        db.SaveChanges();
-                        dBPnls.Add(dBPnl);
-                    }
-
-                    dBStrategyPnl.Pnls = dBPnls;
-
-                    db.StrategyPnls.Update(dBStrategyPnl);
-                    db.SaveChanges();
-                }
-
-            }
+            var strategies = strategyReader._strategies;            
 
         }
     }
