aff0879f5c77b7099189da9cd27feef3bd9411b5
Alom <alom@lothea.co.uk>
2023-05-18 21:39:16 +0100

    save strategies to database

--- GSA/Program.cs
+++ GSA/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace GSA
+﻿using GSA.Data.Context;
+using GSA.models;
+using System.Collections.Generic;
+
+namespace GSA
 {
     public class program
     {
@@ -7,7 +11,61 @@
             var strategyReader = new utils.StrategyReader();
 
             var strategies = strategyReader._strategies;
-  
+
+            SaveToDatabase(strategies);
+        }
+
+        private static void SaveToDatabase(List<Strategy> strategies)
+        {
+            using(var db = new StrategyContext())
+            {
+                db.RemoveRange(db.Capitals);
+                db.RemoveRange(db.Pnls);
+                db.RemoveRange(db.Strategies);
+                db.SaveChanges();
+            }
+            foreach(Strategy strategy in strategies)
+            {
+                using(var db = new StrategyContext())
+                {
+                    GSA.Data.Entity.Strategy dbStrategies = new();
+                    dbStrategies.StratName = strategy.StratName;
+                    dbStrategies.Region = strategy.Region;
+
+                    db.Strategies.Add(dbStrategies);
+                    db.SaveChanges();
+
+                    List<Data.Entity.Pnl> dbPnls = new();
+                    List<Data.Entity.Capital> dbCapitals = new();
+
+                    foreach(var pnl in strategy.Pnl)
+                    {
+                        var dbPnl = new Data.Entity.Pnl() { 
+                            StrategyId = dbStrategies.StrategyId,
+                            Date = pnl.Date,
+                            Amount = pnl.Amount,
+                        };
+
+                        dbPnls.Add(dbPnl);
+                    }
+
+                    foreach(var capital in strategy.Capital)
+                    {
+                        var dbCapital = new Data.Entity.Capital()
+                        {
+                            StrategyId = dbStrategies.StrategyId,
+                            Date = capital.Date,
+                            Amount = capital.Amount
+                        };
+                        
+                        dbCapitals.Add(dbCapital);
+                    }
+
+                    dbStrategies.Capitals = dbCapitals;
+                    dbStrategies.Pnls = dbPnls;
+                    db.SaveChanges();
+                }
+            }
         }
     }
 }
